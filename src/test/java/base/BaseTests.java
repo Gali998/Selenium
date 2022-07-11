11ce2da5635a6cdae89929b760b94e3ae17c9fb7
Mannage <gmannage@wiley.com>
2022-07-11 12:22:59 +0530

    web elements added

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,9 +1,14 @@
 package base;
 
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 
 public class BaseTests {
 
@@ -16,11 +21,17 @@ public class BaseTests {
 
 //        driver.manage().window().maximize();
 //        driver.manage().window().fullscreen();
-        driver.manage().window().setSize(new Dimension(375,812));
+        //driver.manage().window().setSize(new Dimension(375,812));
+
+//        List<WebElement> webElement = (List<WebElement>) driver.findElement(By.tagName("a"));
+//        System.out.println(webElement.size());
+//
+        WebElement inputLink = driver.findElement(By.linkText("Inputs"));
+        inputLink.click();
 
         System.out.println(driver.getTitle());
 
-        driver.quit();
+        //driver.quit();
     }
 
     public static void main(String args[]){
